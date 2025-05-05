e4467572d6278bc407ce7be8698be0117d09a040
JangidVish <vishaljangid_it22@pccoer.in>
2025-05-05 17:03:10 +0530

    Alignment changes

--- src/components/HomePageComponent/Card.jsx
+++ src/components/HomePageComponent/Card.jsx
@@ -3,9 +3,9 @@
 function Card({ image, heading, content }) {
   return (
     <div className=' p-0 md:p-2  md:m-5 rounded-lg overflow-auto md:w-auto w-70 flex flex-col justify-center items-center md:items-start gap-2 '>
-      <div class="text-center">
-        <img src={image} alt="image" className='rounded-lg md:w-70 md:h-50 h-30 w-70' />
-        <div className='md:p-2 font-bold md:text-lg w-40 md:w-auto'>
+      <div className="text-center">
+        <img src={image} alt="image" className='rounded-lg md:w-70 md:h-50 h-50 w-70' />
+        <div className='md:p-2 p-3 font-bold md:text-lg lg:w-40 w-full  md:w-auto'>
           {heading}
         </div>
         <div className='md:p-1 md:w-70 md:text-center  font-semibold '>
--- src/components/HomePageComponent/ProductCard.jsx
+++ src/components/HomePageComponent/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({image, name, link, overview}) => {
     <div className='md:w-80 flex md:flex-col flex-row-reverse
      bg-mid-blue rounded-lg leading-7 items-center md:p-0 px-2'>
     <img src={image} alt="image" className='rounded-lg md:w-80 md:h-60 w-80 h-34'/>
-    <div className='p-2 font-semibold'>
+    <div className='p-2 font-bold text-lg'>
        {name}
        <p className="text-sm font-semibold text-textColor text-grey mt-2 ">{overview}</p>
        <p className=" text-sm mt-2"><a href={link}>View more {"->"}</a></p>
