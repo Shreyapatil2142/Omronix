35aebc346f7be1440b335c01cb548dd9d5f9151c
Shreyapatil2142 <shreyapatil2142@gmail.com>
2025-07-05 11:46:09 +0530

    Error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import ReactDOM from "react-dom/client";
 
 createRoot(document.getElementById('root')).render(
 
     <App />
-
-)
+);
